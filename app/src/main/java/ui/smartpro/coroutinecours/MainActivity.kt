e36dad2f14ad8a59fe0b4ce8e30d9d19ec1471e4
artemternovenko <artem.inkaz@gmail.com>
2021-05-23 14:56:27 +0600

    [UPDATED] вложенная коррутина

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -30,51 +30,40 @@ class MainActivity : AppCompatActivity() {
             onCancel()
         }
     }
-
+//В методе onRun стартуем корутину, а внутри нее еще одну корутину.
     private fun onRun(){
-        log("onRun, start")
+        scope.launch {
+            log("parent coroutine, start")
 
-        job = scope.launch {
-            log("coroutine, start")
-            var x = 0
-            while (x < 5 && isActive) {
-//                TimeUnit.MILLISECONDS.sleep(1000)
-                    delay(1000)
-                //Если isActive начнет возвращать false, то мы прерываем цикл.
-                log("coroutine, ${x++}, isActive=${isActive}")
+            launch {
+                log("child coroutine, start")
+                TimeUnit.MILLISECONDS.sleep(1000)
+                log("child coroutine, end")
             }
-            log("coroutine, end")
+            log("parent coroutine,end")
         }
-
-        log("onRun, end")
     }
-// Когда внутри корутины используется suspend функция, поведение корутины при отмене может
-// отличаться от того, что мы рассмотрели выше.
-// Попробуем заменить TimeUnit.MILLISECONDS.sleep(1000) на delay(1000).
-// Эта suspend функция также делает паузу в указанное количество миллисекунд.
+// Билдер, вызванный в корутине не задерживает выполнение этой корутины.
+// Он только создает и запускает дочернюю корутину, которая уходит работать в свой поток.
+// А родительская корутина продолжает работу.
+//20:32:16.221 parent coroutine, start [DefaultDispatcher-worker-1]
+//20:32:16.222 parent coroutine, end [DefaultDispatcher-worker-1]
+//20:32:16.222 child coroutine, start [DefaultDispatcher-worker-3]
+//20:32:17.224 child coroutine, end [DefaultDispatcher-worker-3]
+//Родительская корутина выполнила весь свой код не дожидаясь выполнения дочерней.
+// А дочерняя корутина в отдельном потоке выполнила свой код.
 //
-//При запуске и отмене корутины логи будут следующими:
-//20:29:33.578 onRun, start [main]
-//20:29:33.606 onRun, end [main]
-//20:29:33.612 coroutine, start [DefaultDispatcher-worker-1]
-//20:29:34.619 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
-//20:29:35.622 coroutine, 1, isActive = true [DefaultDispatcher-worker-3]
-//20:29:36.201 onCancel [main]
-//Отличие от предыдущих примеров в том, что функция delay при отмене корутины сразу прерывает
-// выполнение кода корутина. Метод log после delay не вызывается и
-// до проверки isActive в цикле уже не доходит.
-//
-//Функция delay подписывается на событие отмены корутины, и при возникновении такого события бросает
-// специальный Exception. Именно поэтому выполнение кода корутины прерывается на delay и дальше не идет.
-// Но этот Exception ловится и обрабатывается внутренними механизмами корутины,
-// поэтому никакого крэша не происходит.
-//
-//Такую специфику имеет не все suspend функции, а только cancellable.
+//Хоть родительская корутина и выполнила сразу же весь свой код,
+// но ее статус поменяется на Завершена только когда выполнится дочерняя корутина.
+// Потому что родительская корутина подписывается на дочерние и ждет их завершения,
+// прежде чем официально завершиться. Т.е. то, что корутина выполнила свой код, может и не означать,
+// что она имеет статус Завершена.
+
 
 
     private fun onCancel(){
         log("onCancel")
-        job.cancel()
+//        job.cancel()
     }
 
     override fun onDestroy() {
@@ -82,17 +71,6 @@ class MainActivity : AppCompatActivity() {
         log("onDestroy")
         scope.cancel()
     }
-// Попробуем нажать кнопку Run и через пару секунд закрыть приложение.
-//11:32:14.242 onRun, start [main]
-//11:32:14.284 onRun, end [main]
-//11:32:14.293 coroutine, start [DefaultDispatcher-worker-1]
-//11:32:15.294 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
-//11:32:16.295 coroutine, 1, isActive = true [DefaultDispatcher-worker-1]
-//11:32:16.355 onDestroy [main]
-//11:32:17.297 coroutine, 2, isActive = false [DefaultDispatcher-worker-1]
-//11:32:17.297 coroutine, end [DefaultDispatcher-worker-1]
-//
-//Логи показывают, что корутина была отменена. Scope сделал свое дело.
 
     private fun log(text: String) {
         Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
