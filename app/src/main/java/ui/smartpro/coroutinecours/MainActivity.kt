af5f6b2ffbf9f540b4bdc8a9b8861b74d8f6076c
artemternovenko <artem.inkaz@gmail.com>
2021-05-23 13:58:45 +0600

    [UPDATED] onRun- onCancel

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -37,22 +37,34 @@ class MainActivity : AppCompatActivity() {
     private fun onRun(){
         log("onRun, Start")
 //используется фоновый поток из диспетчера по умолчанию
-        scope.launch {
-            log("coroutine, start")
-            TimeUnit.MILLISECONDS.sleep(1000)
-            log("coroutine, end")
-        }
-
+       job = scope.launch {
+           log("coroutine, start")
+           var x = 0
+           while (x < 5) {
+               TimeUnit.MILLISECONDS.sleep(1000)
+               log("coroutine, ${x++}")
+           }
+           log("coroutine, end")
+       }
         log("onRun, end")
     }
-//11:00:55.994 onRun, start [main] полностью выполнился еще до того, как начала работать корутина
-//11:00:56.062 onRun, end [main] основной код идет дальше, и метод onRun завершается.
-//11:00:56.075 coroutine, start [DefaultDispatcher-worker-1] Т.е. билдер launch не блокирует поток, где он был запущен. Он создает корутину и отправляет ее на запуск в отдельном потоке (это видно по логам coroutine)
-//11:00:57.076 coroutine, end [DefaultDispatcher-worker-1]
+//11:24:53.416 onRun, start [main]
+//11:24:53.466 onRun, end [main]
+//11:24:53.474 coroutine, start [DefaultDispatcher-worker-1]
+//11:24:54.475 coroutine, 0 [DefaultDispatcher-worker-1]
+//11:24:55.476 coroutine, 1 [DefaultDispatcher-worker-1]
+//11:24:56.135 onCancel [main]
+//11:24:56.480 coroutine, 2 [DefaultDispatcher-worker-1]
+//11:24:57.484 coroutine, 3 [DefaultDispatcher-worker-1]
+//11:24:58.486 coroutine, 4 [DefaultDispatcher-worker-1]
+//11:24:58.486 coroutine, end [DefaultDispatcher-worker-1]
 
 
 
-    private fun onCancel(){}
+    private fun onCancel(){
+        log("onCancel")
+        job.cancel()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
