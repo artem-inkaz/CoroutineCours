9e6d8ff41214c25786d1b551a17e9ecf23c91a8c
artemternovenko <artem.inkaz@gmail.com>
2021-05-23 14:19:01 +0600

    [UPDATED] delay

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -38,7 +38,8 @@ class MainActivity : AppCompatActivity() {
             log("coroutine, start")
             var x = 0
             while (x < 5 && isActive) {
-                TimeUnit.MILLISECONDS.sleep(1000)
+//                TimeUnit.MILLISECONDS.sleep(1000)
+                    delay(1000)
                 //Если isActive начнет возвращать false, то мы прерываем цикл.
                 log("coroutine, ${x++}, isActive=${isActive}")
             }
@@ -47,18 +48,28 @@ class MainActivity : AppCompatActivity() {
 
         log("onRun, end")
     }
-// После нажатия на кнопку Cancel, метод isActive начинает возвращать false.
-// Это означает, что корутина больше не активна. Ее выполнение или ее результат более никому не нужны.
-// Можно прервать ее работу, что мы и делаем в условии цикла.
-//11:27:45.001 onRun, start [main]
-//11:27:45.048 onRun, end [main]
-//11:27:45.053 coroutine, start [DefaultDispatcher-worker-1]
-//11:27:46.053 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
-//11:27:47.055 coroutine, 1, isActive = true [DefaultDispatcher-worker-1]
-//11:27:47.665 onCancel [main]
-//11:27:48.057 coroutine, 2, isActive = false [DefaultDispatcher-worker-1]
-//11:27:48.057 coroutine, end [DefaultDispatcher-worker-1]
-
+// Когда внутри корутины используется suspend функция, поведение корутины при отмене может
+// отличаться от того, что мы рассмотрели выше.
+// Попробуем заменить TimeUnit.MILLISECONDS.sleep(1000) на delay(1000).
+// Эта suspend функция также делает паузу в указанное количество миллисекунд.
+//
+//При запуске и отмене корутины логи будут следующими:
+//20:29:33.578 onRun, start [main]
+//20:29:33.606 onRun, end [main]
+//20:29:33.612 coroutine, start [DefaultDispatcher-worker-1]
+//20:29:34.619 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
+//20:29:35.622 coroutine, 1, isActive = true [DefaultDispatcher-worker-3]
+//20:29:36.201 onCancel [main]
+//Отличие от предыдущих примеров в том, что функция delay при отмене корутины сразу прерывает
+// выполнение кода корутина. Метод log после delay не вызывается и
+// до проверки isActive в цикле уже не доходит.
+//
+//Функция delay подписывается на событие отмены корутины, и при возникновении такого события бросает
+// специальный Exception. Именно поэтому выполнение кода корутины прерывается на delay и дальше не идет.
+// Но этот Exception ловится и обрабатывается внутренними механизмами корутины,
+// поэтому никакого крэша не происходит.
+//
+//Такую специфику имеет не все suspend функции, а только cancellable.
 
 
     private fun onCancel(){
