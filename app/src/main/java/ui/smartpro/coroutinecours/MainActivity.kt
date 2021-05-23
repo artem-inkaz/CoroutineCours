de5e48cb16c6ce9fbc928ba260ea092006b394e9
artemternovenko <artem.inkaz@gmail.com>
2021-05-23 14:13:32 +0600

    [UPDATED] статус isActivw

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -37,35 +34,30 @@ class MainActivity : AppCompatActivity() {
     private fun onRun(){
         log("onRun, start")
 
-        scope.launch {
+        job = scope.launch {
             log("coroutine, start")
-            TimeUnit.MILLISECONDS.sleep(1000)
+            var x = 0
+            while (x < 5 && isActive) {
+                TimeUnit.MILLISECONDS.sleep(1000)
+                //Если isActive начнет возвращать false, то мы прерываем цикл.
+                log("coroutine, ${x++}, isActive=${isActive}")
+            }
             log("coroutine, end")
-        }
-
-        log("onRun, middle")
-
-        scope.launch {
-            log("coroutine2, start")
-            TimeUnit.MILLISECONDS.sleep(1500)
-            log("coroutine2, end")
         }
 
         log("onRun, end")
     }
-//Метод onRun быстро выполнился, создав и отправив на выполнение пару корутин.
-// Корутины стартовали практически одновременно, в отдельных потоках сделали свою работу и завершились.
-// Между собой они никак не связаны.
-//
-//Эти примеры показывают, что билдер launch быстро выполняется, не блокируя и не задерживая основной поток.
-// А корутины уходят в отдельный поток и выполняются там.
-//11:07:54.350 onRun, start [main]
-//11:07:54.351 onRun, middle [main]
-//11:07:54.352 onRun, end [main]
-//11:07:54.352 coroutine2, start [DefaultDispatcher-worker-4]
-//11:07:54.354 coroutine, start [DefaultDispatcher-worker-1]
-//11:07:55.355 coroutine, end [DefaultDispatcher-worker-1]
-//11:07:55.855 coroutine2, end [DefaultDispatcher-worker-4]
+// После нажатия на кнопку Cancel, метод isActive начинает возвращать false.
+// Это означает, что корутина больше не активна. Ее выполнение или ее результат более никому не нужны.
+// Можно прервать ее работу, что мы и делаем в условии цикла.
+//11:27:45.001 onRun, start [main]
+//11:27:45.048 onRun, end [main]
+//11:27:45.053 coroutine, start [DefaultDispatcher-worker-1]
+//11:27:46.053 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
+//11:27:47.055 coroutine, 1, isActive = true [DefaultDispatcher-worker-1]
+//11:27:47.665 onCancel [main]
+//11:27:48.057 coroutine, 2, isActive = false [DefaultDispatcher-worker-1]
+//11:27:48.057 coroutine, end [DefaultDispatcher-worker-1]
 
 
 
@@ -79,6 +71,17 @@ class MainActivity : AppCompatActivity() {
         log("onDestroy")
         scope.cancel()
     }
+// Попробуем нажать кнопку Run и через пару секунд закрыть приложение.
+//11:32:14.242 onRun, start [main]
+//11:32:14.284 onRun, end [main]
+//11:32:14.293 coroutine, start [DefaultDispatcher-worker-1]
+//11:32:15.294 coroutine, 0, isActive = true [DefaultDispatcher-worker-1]
+//11:32:16.295 coroutine, 1, isActive = true [DefaultDispatcher-worker-1]
+//11:32:16.355 onDestroy [main]
+//11:32:17.297 coroutine, 2, isActive = false [DefaultDispatcher-worker-1]
+//11:32:17.297 coroutine, end [DefaultDispatcher-worker-1]
+//
+//Логи показывают, что корутина была отменена. Scope сделал свое дело.
 
     private fun log(text: String) {
         Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
