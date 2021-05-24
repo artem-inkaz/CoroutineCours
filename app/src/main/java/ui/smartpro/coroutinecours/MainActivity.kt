7b407dfbd70901e1f4c77795669b05b1b1446329
artemternovenko <artem.inkaz@gmail.com>
2021-05-24 16:22:10 +0600

    [UPDATED] Main

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -11,19 +11,17 @@ import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
-import kotlin.coroutines.ContinuationInterceptor
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
-import kotlin.coroutines.coroutineContext
+import kotlin.concurrent.thread
+import kotlin.coroutines.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     //указываем время
     private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
 
-    private val scope = CoroutineScope(Job())
+    private val scope = CoroutineScope(Dispatchers.Main)
 
-    lateinit var job: Job
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,62 +42,56 @@ class MainActivity : AppCompatActivity() {
 
 private fun onRun() {
 
-    val scope = CoroutineScope(
-            Executors.newSingleThreadExecutor().asCoroutineDispatcher()
-    )
+        if (job?.isActive == true) return
+        job = scope.launch {
+            log("start coroutine")
+            val data = getData()
+            log("end coroutine")
+            updateUI(data)
 
-    repeat(6) {
-        scope.launch {
-            log("coroutine $it, start")
-            TimeUnit.MILLISECONDS.sleep(100)
-            log("coroutine $it, end")
         }
-    }
 }
-//6 корутин придут к диспетчеру, у которого есть только один поток.
-//
-//Логи:
-//
-//20:26:39.502 coroutine 0, start [pool-1-thread-1]
-//20:26:39.604 coroutine 0, end [pool-1-thread-1]
-//20:26:39.605 coroutine 1, start [pool-1-thread-1]
-//20:26:39.706 coroutine 1, end [pool-1-thread-1]
-//20:26:39.707 coroutine 2, start [pool-1-thread-1]
-//20:26:39.808 coroutine 2, end [pool-1-thread-1]
-//20:26:39.808 coroutine 3, start [pool-1-thread-1]
-//20:26:39.911 coroutine 3, end [pool-1-thread-1]
-//20:26:39.911 coroutine 4, start [pool-1-thread-1]
-//20:26:40.013 coroutine 4, end [pool-1-thread-1]
-//20:26:40.013 coroutine 5, start [pool-1-thread-1]
-//20:26:40.114 coroutine 5, end [pool-1-thread-1]
-//
-//Корутинам пришлось выстраиваться в очередь и выполнятся последовательно.
-
 
 //простой метод, чтобы доставать из контекста и выводить в лог Job и диспетчер.
 // Это поможет нам наглядно понять, что происходит с контекстом
     private fun contextToString(context: CoroutineContext) : String =
             "Job = ${context[Job]}, Dispatchers = ${context[ContinuationInterceptor]}"
 
-    private suspend fun getData(): String{
-        delay(1000)
-        return "data"
-    }
+//suspend функция приостанавливает код корутины, выполняет свою работу в фоновом потоке,
+// а затем возобновляет корутину. Поток корутины при этом не блокируется.
+// suspend функция код должен быть запущен в корутине, и вне корутины ее не запустить.
+    private suspend fun getData(): String =
+        suspendCoroutine {
+            //покажет из какого потока была запущена эта функция
+            log("suspend function, start")
+            thread {
+                //в каком потоке она выполняет свою работу
+                log("suspend function, background work")
+                TimeUnit.MILLISECONDS.sleep(3000)
+                it.resume("Data!")
+            }
+        }
+
 
     private suspend fun getData2(): String {
         delay(1500)
         return "data2"
     }
 
+//Данные, которые будут получены сразу отображаем на экране.
+    private fun updateUI(data:String) {
+        label.text = data
+    }
+
     private fun onRun2(){
         log("onRun2, start")
-        job.start()
+//        job?.start()
         log("onRun2, end")
     }
 
     private fun onCancel(){
         log("onCancel")
-        job.cancel()
+//        job?.cancel()
     }
 
     override fun onDestroy() {
