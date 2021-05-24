7996385941fe1a3af7192784535908c0a1fbfa6f
artemternovenko <artem.inkaz@gmail.com>
2021-05-24 09:53:05 +0600

    [UPDATED] паралелльная работа 2-х suspend fun c async

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -34,26 +34,39 @@ class MainActivity : AppCompatActivity() {
             onCancel()
         }
     }
-//В корутине запускаем две suspend функции: getData и getData2.
-// Первая имитирует выполнение работы в течение 1000 мс и возвращает строку “data”,
-// а вторая - в течение 1500 мс и возвращает строку “data2”.
+//С помощью async мы можем вызвать эти функции параллельно, чтобы сократить общее время их выполнения.
+// Помещаем вызов каждой suspend функции в отдельную async корутину.
+// И методом await получаем результат.
     private fun onRun() {
 
     scope.launch {
         log("parent coroutine, start")
-        val data = getData()
-        val data2 = getData2()
-        val result = "${data}, ${data2}"
+        val data = async { getData()}
+        val data2 = async { getData2()}
+        log("parent coroutine, wait until children return result")
+        val result = "${data.await()}, ${data2.await()}"
         log("parent coroutine, children returned: $result")
         log("parent coroutine, end")
     }
 }
-//11:06:35.026 parent coroutine, start [DefaultDispatcher-worker-1]
-//11:06:37.543 parent coroutine, children returned: data, data2 [DefaultDispatcher-worker-1]
-//11:06:37.546 parent coroutine, end [DefaultDispatcher-worker-1]
+//Логи:
 //
-//По времени в логах видно, что общее время выполнения корутины составило около 2500 мсек.
-// Это верно, т.к. suspend функции были вызваны последовательно одна за другой.
+//11:00:52.751 parent coroutine, start [DefaultDispatcher-worker-1]
+//11:00:52.758 parent coroutine, wait until children return result [DefaultDispatcher-worker-1]
+//11:00:54.268 parent coroutine, children returned: data, data2 [DefaultDispatcher-worker-3]
+//11:00:54.269 parent coroutine, end [DefaultDispatcher-worker-3]
+//
+//Теперь логи показывают, что общее время выполнения родительской корутины составило около 1500 мсек.
+// Наши suspend функции выполнялись параллельно в разных async корутинах,
+// а родительская корутина дождалась выполнения обеих и получила результаты выполнения.
+//
+//А теперь снова представьте, что родительская корутина выполняется в main потоке.
+// А suspend-функции - это получение данных с сервера с помощью, например, Retrofit.
+// Вы выполняете запросы в фоновых потоках, получаете данные в main потоке и
+// отображаете их не в логе, а на экране. И никаких колбэков.
+// В этом и есть одна из основных возможностей корутин - лаконичный асинхронный код.
+//Lazy
+//async корутину также можно запускать в режиме Lazy. Метод await стартует ее выполнение.
 
     private suspend fun getData(): String{
         delay(1000)
