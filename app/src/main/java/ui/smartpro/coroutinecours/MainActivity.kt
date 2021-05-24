eca0a84bbd19b21701e2afa81a9f5a5a5e3a6cf3
artemternovenko <artem.inkaz@gmail.com>
2021-05-24 09:38:50 +0600

    [UPDATED] async

--- app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
+++ app/src/main/java/ui/smartpro/coroutinecours/MainActivity.kt
@@ -35,29 +35,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 //В методе onRun стартуем корутину, а внутри нее еще одну корутину.
-    private fun onRun(){
-        log("onRun, start")
-    // корутину c параметром start = LAZY.
-    // Это не даст корутине начать работу сразу после создания.
-    // В примере стартуем в onRun2()
-        job = scope.launch (start = CoroutineStart.LAZY) {
-            log("coroutine, start")
-            TimeUnit.MILLISECONDS.sleep(1000)
-            log("coroutine, end")
-        }
-            log("onRun, end")
-    }
+    private fun onRun() {
 
-//11:28:30.526 onRun, start [main]
-//11:28:30.532 onRun, end [main]
-//11:28:32.317 onRun2, start [main]
-//11:28:32.321 onRun2, end [main]
-//11:28:32.328 coroutine, start [DefaultDispatcher-worker-1]
-//11:28:33.331 coroutine, end [DefaultDispatcher-worker-1]
+    scope.launch {
+        log("parent coroutine, start")
+//создаем и стартуем дочернюю корутину. Билдер возвращает нам Deferred объект
+        val deferred = async() {
+            log("child coroutine, start")
+            TimeUnit.MILLISECONDS.sleep(1000)
+            "async result"
+        }
+        log("parent coroutine, wait until child returns result")
+// метод await приостановит выполнение родительской корутины, пока не выполнится дочерняя,
+// и вернет результат дочерней корутины.
+// Дочерняя корутина имитирует работу в течение одной секунды, а затем возвращает строку "async result".
+        val result = deferred.await()
+        log("parent coroutine, child returns: $result")
+        log("parent coroutine, end")
+    }
+}
+//parent coroutine, start [DefaultDispatcher-worker-1]
+//parent coroutine, wait until child returns result [DefaultDispatcher-worker-1]
+//child coroutine, start [DefaultDispatcher-worker-2]
+//child coroutine, end [DefaultDispatcher-worker-2]
+//parent coroutine, child returns: async result [DefaultDispatcher-worker-2]
+//parent coroutine, end [DefaultDispatcher-worker-2]
 //
-//Жмем кнопку Run. Метод onRun создает корутину и завершается. Но корутина не стартует.
-// Через пару секунд жмем кнопку Run2, и корутина начинает работу. Метод start ничего не блокирует.
-// Он стартует корутину в отдельном потоке, а метод onRun2 быстро завершается.
+//Все аналогично примеру с launch+join. Билдер async создает и запускает дочернюю корутину.
+// Родительская корутина продолжает выполняться и останавливается на методе await.
+// Теперь она в ожидании завершения дочерней корутины.
+//
+//Дочерняя корутина выполняется в отдельном потоке.
+// По ее завершению метод await возвращает результат ее работы (строка “async result”), и
+// возобновляет выполнение кода родительской корутины.
+
+
     private fun onRun2(){
         log("onRun2, start")
         job.start()
